6c9d654938a69b4e40c649ae346e7b74da59b8df
tatielirb <tatielirb@gmail.com>
2024-01-26 13:26:53 -0300

    feat: adicionando css do login e iniciando a estilizacao

--- google-home-page/src/pages/Login/Login.tsx
+++ google-home-page/src/pages/Login/Login.tsx
@@ -1,28 +1,35 @@
 import React from "react";
+import "./Login.css";
 import Button from "../../components/Button/Index.tsx";
 
 export default function Login() {
   return (
     <div className="login">
-      <div className="login-header">
-        <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Google" />
-        <h3>Fazer Login</h3>
-        <p>Use sua conta do Google</p>
-      </div>
-      <div className="login-form">
-        <input type="text" name="" id="" />
-        <a href="">Esqueceu seu e-mail?</a>
-      </div>
-      <div className="login-infos">
-        <p>
-          Não está no seu computador? Use o modo visitante para fazer login com
-          privacidade.
-        </p>
-        <a href="">Saiba mais sobre como usar o Modo visitante</a>
-      </div>
-      <div className="login-button">
-        <a href="">Criar conta</a>
-        <Button title="Avançar" />
+      <div className="login-content">
+        <div className="login-header">
+          <img
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt="Google"
+            className="login-img"
+          />
+          <span>Fazer Login</span>
+          <p>Use sua conta do Google</p>
+        </div>
+        <div className="login-form">
+          <input type="text" name="" id="" />
+          <a href="">Esqueceu seu e-mail?</a>
+        </div>
+        <div className="login-infos">
+          <p>
+            Não está no seu computador? Use o modo visitante para fazer login
+            com privacidade.
+          </p>
+          <a href="">Saiba mais sobre como usar o Modo visitante</a>
+        </div>
+        <div className="login-button">
+          <a href="">Criar conta</a>
+          <Button title="Avançar" />
+        </div>
       </div>
     </div>
   );
