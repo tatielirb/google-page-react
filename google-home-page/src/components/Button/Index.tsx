ace450f9dff63494e6ccec8f99b57feb54709d20
tatielirb <tatielirb@gmail.com>
2024-01-29 14:48:01 -0300

    feat: adiconando prop no componente button

--- google-home-page/src/components/Button/Index.tsx
+++ google-home-page/src/components/Button/Index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Button.css";
-import { buttonProps } from '../../types/Button.ts';
+import { buttonProps } from "../../types/Button.ts";
 
-export default function Button({ title, classNameType }: buttonProps) {
-  return <button className={ classNameType }>{title}</button>;
+export default function Button({
+  title,
+  classNameType,
+  onClickProp,
+}: buttonProps) {
+  return (
+    <button className={classNameType} onClick={onClickProp}>
+      {title}
+    </button>
+  );
 }
--- google-home-page/src/types/Button.ts
+++ google-home-page/src/types/Button.ts
@@ -1,5 +1,6 @@
 export interface buttonProps {
   title: string;
   classNameType?: string;
+  onClickProp?: () => void;
 }
 
