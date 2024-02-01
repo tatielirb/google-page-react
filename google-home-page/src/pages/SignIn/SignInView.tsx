0acd5738103fd1e3b02dd128a2b4a01f8f0f015b
tatielirb <tatielirb@gmail.com>
2024-02-01 08:53:03 -0300

    feat: ajustando condicional para ficar mais clean

--- google-home-page/src/pages/SignIn/SignInView.tsx
+++ google-home-page/src/pages/SignIn/SignInView.tsx
@@ -22,72 +22,79 @@ export default function SignInView() {
       <div className="login-content">
         <img
           src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-          alt="Google"
+          alt="Google Logo"
           className="login-img"
         />
 
-        <div className={`login-step ${activeBlock === 1 ? "active" : ""}`}>
-          <div className="login-header">
-            <h1>Fazer Login</h1>
-            <p>Use sua conta do Google</p>
-          </div>
-
-          <div className="login-form">
-            <div className="login-form--email">
-              <Input
-                typeProp="text"
-                placeholderProp="E-mail e Telefone"
-                valueProp={valueEmail}
-                onChangeProp={(valueEmail) => getValueEmail(valueEmail)}
-                requiredProp
-              ></Input>
-
-              <Link title="Esqueceu seu e-mail?"></Link>
+        {activeBlock === 1 && (
+          <div className="login-step active">
+            <div className="login-header">
+              <h1>Fazer Login</h1>
+              <p>Use sua conta do Google</p>
             </div>
-          </div>
-          <div className="login-infos">
-            <p>
-              Não está no seu computador? Use o modo visitante para fazer login
-              com privacidade.
-            </p>
-            <Link title="Saiba mais sobre como usar o Modo visitante"></Link>
-          </div>
-          <div className="login-button">
-            <Button title="Criar conta" classNameType="button-white" />
-            <Button
-              title="Avançar"
-              classNameType="button-blue"
-              onClickProp={() => handleClick(2)}
-            />
-          </div>
-        </div>
 
-        <div className={`login-step ${activeBlock === 2 ? "active" : ""}`}>
-          <div className="login-header">
-            <h1>Olá!</h1>
-            <div className="login-header--email">{valueEmail}</div>
-          </div>
-          <div className="login-form">
-            <div className="login-form--password">
-              <Input
-                typeProp="password"
-                placeholderProp="Digite sua senha"
-              ></Input>
-              <div className="login-form--checkbox">
-                <input type="checkbox" id="checkbox" name="checkbox" />
-                <label htmlFor="checkbox">Mostrar senha</label>
+            <div className="login-form">
+              <div className="login-form--email">
+                <Input
+                  typeProp="text"
+                  placeholderProp="E-mail e Telefone"
+                  valueProp={valueEmail}
+                  onChangeProp={(valueEmail) => getValueEmail(valueEmail)}
+                  requiredProp
+                ></Input>
+
+                <Link title="Esqueceu seu e-mail?"></Link>
               </div>
             </div>
+            <div className="login-infos">
+              <p>
+                Não está no seu computador? Use o modo visitante para fazer
+                login com privacidade.
+              </p>
+              <Link title="Saiba mais sobre como usar o Modo visitante"></Link>
+            </div>
+            <div className="login-button">
+              <Button title="Criar conta" classNameType="button-white" />
+              <Button
+                title="Avançar"
+                classNameType="button-blue"
+                onClickProp={() => handleClick(2)}
+              />
+            </div>
           </div>
-          <div className="login-button">
-            <Button title="Tente de outro jeito" classNameType="button-white" />
-            <Button
-              title="Avançar"
-              classNameType="button-blue"
-              onClickProp={() => handleClick(1)}
-            />
+        )}
+
+        {activeBlock === 2 && (
+          <div className="login-step active">
+            <div className="login-header">
+              <h1>Olá!</h1>
+              <div className="login-header--email">{valueEmail}</div>
+            </div>
+            <div className="login-form">
+              <div className="login-form--password">
+                <Input
+                  typeProp="password"
+                  placeholderProp="Digite sua senha"
+                ></Input>
+                <div className="login-form--checkbox">
+                  <input type="checkbox" id="checkbox" name="checkbox" />
+                  <label htmlFor="checkbox">Mostrar senha</label>
+                </div>
+              </div>
+            </div>
+            <div className="login-button">
+              <Button
+                title="Tente de outro jeito"
+                classNameType="button-white"
+              />
+              <Button
+                title="Avançar"
+                classNameType="button-blue"
+                onClickProp={() => handleClick(1)}
+              />
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
